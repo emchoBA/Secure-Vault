50393463b011c3640ab19ce6f39193ec1b38b128
emchoBA <93426572+emchoBA@users.noreply.github.com>
2024-12-08 19:00:09 +0300

    Modified Encryption.java class.

--- src/Encryption.java
+++ src/Encryption.java
@@ -1,31 +1,48 @@
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Base64;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileReader;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Encryption {
     private static final String ALGO = "AES";
     private static final String TRANSFORM = "AES/CBC/PKCS5Padding"; // transformation for usage of iv and padding
     private static final int KEY_SIZE = 256; //AES-256 key size
-    private static final int BLOCK_SIZE = 16; // AES block size
+    private static final int BLOCK_SIZE = 16; // IV block size
 
-//    public static SecretKey genKey(){
-//        try {
-//            KeyGenerator keyG = KeyGenerator.getInstance(ALGO);
-//            keyG.init(KEY_SIZE);
-//            return keyG.generateKey();
-//        } catch (NoSuchAlgorithmException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-    // USE PASSWORD DERIVATION FOR ENC KEY
+    public SecretKey genKey() throws NoSuchAlgorithmException{
+            KeyGenerator keyG = KeyGenerator.getInstance(ALGO);
+            keyG.init(KEY_SIZE);
+            return keyG.generateKey();
+        }
+     //!!!!USE PASSWORD DERIVATION FOR ENC KEY
 
     public static IvParameterSpec genIv(){ // can be byte[] genIv()
         byte[] iv = new byte[BLOCK_SIZE];
         new SecureRandom().nextBytes(iv); // like generating salt, cbc
         return new IvParameterSpec(iv);
+    }
+
+    public void saveKey(SecretKey key, String path) throws IOException {
+        String encKey = Base64.getEncoder().encodeToString(key.getEncoded());
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+        writer.write(encKey);
+        writer.close();
+    }
+
+    public SecretKey loadKey(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        String encKey = reader.readLine();
+        byte[] key = Base64.getDecoder().decode(encKey);
+        return new SecretKeySpec(key, ALGO);
     }
 
     public static byte[] encrypt(byte[] data, SecretKey key, IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException { //can be String input
